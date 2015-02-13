fb1f747582bd8217b67e4b4f16a13d33e54177d2
daywrite@yeah.net <daywrite@yeah.net>
2015-02-13 17:37:39 +0800

    Redis阶段

---
+++ OBone.Redis.Tests/RedisTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Diagnostics;
+
+namespace OBone.Redis.Tests
+{
+    [TestClass]
+    public class RedisTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var client=new Client.
+        }
+    }
+}
