84975c83c1e2bf2c1cf8375a4f76e2c2a2ffe069
daywrite@yeah.net <daywrite@yeah.net>
2015-02-13 12:53:55 +0800

    统一

---
+++ OBone.API/Controllers/CommunityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using OBone.Application;
+using OBone.Core.Models;
+
+namespace OBone.API.Controllers
+{
+    [RoutePrefix("api/Community")]
+    public class CommunityController : ApiController
+    {
+        private readonly ICommunityContract _communityContract;
+        public CommunityController(ICommunityContract communityContract)
+        {
+            _communityContract = communityContract;
+        }
+
+        [HttpGet]
+        [Route("Query/All")]
+        public IQueryable<Community> Get()
+        {
+            return _communityContract.Communities;
+        }
+    }
+}
