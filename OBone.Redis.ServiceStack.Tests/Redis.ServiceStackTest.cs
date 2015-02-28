d21b455defdda6822c1c8f00438a7ea4ff822c4c
daywrite@yeah.net <daywrite@yeah.net>
2015-02-28 18:58:40 +0800

    ServiceStack.Test

---
+++ OBone.Redis.ServiceStack.Tests/Redis.ServiceStackTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OBone.Core.Data.Entity;
+using OBone.Core.Models;
+using ServiceStack.Redis;
+
+namespace OBone.Redis.ServiceStack.Tests
+{
+    [TestClass]
+    public class ServiceStackTest
+    {
+        OBoneDbContext context = new OBoneDbContext();
+
+        public RedisClient Client = null;
+
+        public ServiceStackTest()
+        {
+            Client = new RedisClient("127.0.0.1", 6379);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string key = "test";
+            string value = "OBone.Redis";
+
+            string str = Client.Get<string>(key);
+
+            Assert.AreEqual(value, str);
+        }
+
+        [TestMethod]
+        public void TestList()
+        {
+            Client.FlushAll();
+            var rCommunities = Client.As<Community>();
+
+            List<Community> Entities = context.Set<Community>().ToList();
+
+            rCommunities.StoreAll(Entities.ToList());
+        }
+
+        [TestMethod]
+        public void GetList()
+        {
+            var rCommunities = Client.As<Community>();
+
+            var result = rCommunities.GetAll();
+        }
+    }
+}
