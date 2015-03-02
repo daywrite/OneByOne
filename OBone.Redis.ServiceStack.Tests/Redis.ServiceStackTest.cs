6e5a96222f59d2513c93b74d48707e97d07158be
daywrite@yeah.net <daywrite@yeah.net>
2015-03-02 11:32:41 +0800

    Redis多线程失败

--- OBone.Redis.ServiceStack.Tests/Redis.ServiceStackTest.cs
+++ OBone.Redis.ServiceStack.Tests/Redis.ServiceStackTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OBone.Core.Data.Entity;
 using OBone.Core.Models;
@@ -36,13 +37,48 @@ namespace OBone.Redis.ServiceStack.Tests
         public void TestList()
         {
             Client.FlushAll();
-            var rCommunities = Client.As<Community>();
+
+            //var rCommunities = Client.As<Community>();
 
             List<Community> Entities = context.Set<Community>().ToList();
-
-            rCommunities.StoreAll(Entities.ToList());
+            Task.Run(() => {
+                RedisClient _Client = new RedisClient("127.0.0.1", 6379);
+                var rCommunities = _Client.As<Community>();
+                List<Community> Entities10 = Entities.Take(1000).ToList();
+                rCommunities.StoreAll(Entities10);
+            });
+            //Task.Run(() =>
+            //{
+            //    List<Community> Entities20 = Entities.Skip(1000).Take(1000).ToList();
+            //    rCommunities.StoreAll(Entities20);
+            //});
+            //Task.Run(() =>
+            //{
+            //    List<Community> Entities30 = Entities.Skip(2000).Take(1000).ToList();
+            //    rCommunities.StoreAll(Entities30);
+            //});
+            //Task.Run(() =>
+            //{
+            //    List<Community> Entities40 = Entities.Skip(3000).Take(1000).ToList();
+            //    rCommunities.StoreAll(Entities40);
+            //});
+            //Task.Run(() =>
+            //{
+            //    List<Community> Entities50 = Entities.Skip(4000).Take(1000).ToList();
+            //    rCommunities.StoreAll(Entities50);
+            //});         
+            //rCommunities.StoreAll(Entities1020);
         }
-
+        public static PooledRedisClientManager CreateManager(string[] readWriteHosts, string[] readOnlyHosts)
+        {
+            //支持读写分离，均衡负载
+            return new PooledRedisClientManager(readWriteHosts, readOnlyHosts, new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 5,//“写”链接池链接数
+                MaxReadPoolSize = 5,//“写”链接池链接数
+                AutoStart = true,
+            });
+        }
         [TestMethod]
         public void GetList()
         {
